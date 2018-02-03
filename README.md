5773cdc07ba1707adbb8c1e07307e2397ca8c8a6
Samantha McCall <samanthaadams.biz@gmail.com>
2018-02-03 15:13:26 -0600

    adding where I got to on gamae

---
+++ README.md
@@ -0,0 +1,11 @@
+Responsive-Portfolio
+
+Object
+
+The goal of this game is to display empty spaces for the name of a cartoon character, which the player has seven guesses to solve.
+
+JavaScript used
+
+I created an array (tried to use array that matched a name with an image but everytime I used it I broke the game). The player's guesses count down or go in the word. If they miss seven letters, the solution is revealed in red. If they win, the solution is revealed in green (and here is where the image would show up).
+
+I look forward to updating this to say the game is working COMPLETELY. 
---
+++ assets/javascript/game.js
@@ -0,0 +1,152 @@
+// On load function
+var challengeWordElement = document.getElementById("challenge-word");
+var guessesElement = document.getElementById("guesses");
+var wordBank = ["ariel", "tom", "patrick", "bart", "stimpy"];
+var userKeys = [];
+var hangmanProgress = [];
+var random = Math.floor(Math.random() * wordBank.length);
+var challengeWord = wordBank[random];
+var blankSpace = "_";
+var guessesLeft = 0;
+var guessesLeftElement = document.getElementById("guesses-left");
+var scoreChecker = 0;
+var solution = document.getElementById("solution");
+var loss = document.getElementById("loss");
+var win = document.getElementById("win");
+var resetButton = document.getElementById("reset");
+var winCountElement = document.getElementById("win-count");
+var lossCountElement = document.getElementById("loss-count");
+var lossCount = 0;
+var winCount = 0;
+
+function load() {
+    guessesLeft = 7;
+    guessesLeftElement.innerHTML = guessesLeft;
+
+    for (i = 0; i < challengeWord.length; i++) {
+        hangmanProgress.push(blankSpace);
+    }
+
+    challengeWordElement.innerHTML = hangmanProgress.join(" ");
+    console.log(hangmanProgress);
+
+
+    lossCountElement.innerHTML = lossCount;
+    winCountElement.innerHTML = winCount;
+};
+
+// Run load function
+load();
+
+
+// On key press
+document.onkeyup = function (event) {
+    console.log(event.key);
+    console.log(challengeWord);
+
+    // If key is within lowercase alphabet
+    if (event.keyCode < 91 && event.keyCode > 64) {
+
+        if (userKeys.includes(event.key)) {
+            // do nothing
+        } else {
+            // add keys to display array
+            userKeys.push(event.key);
+            guessesElement.innerHTML = userKeys.join(" ");
+
+        };
+
+        // Check against chosen word
+        for (i = 0; i < challengeWord.length; i++) {
+            if (event.key === challengeWord[i]) {
+                hangmanProgress[i] = event.key;
+                challengeWordElement.innerHTML = hangmanProgress.join(" ");
+
+                console.log(hangmanProgress);
+            } else {
+                scoreChecker++;
+            }
+
+        };
+
+        // if no keys match lose one guessesLeft
+        if (scoreChecker === challengeWord.length) {
+            guessesLeft -= 1;
+            guessesLeftElement.innerHTML = guessesLeft;
+            console.log(guessesLeft);
+        }
+
+    };
+
+    scoreChecker = 0;
+    // Guesses left gif states
+    if (guessesLeft < 5 && guessesLeft > 3) {
+        show(normalGif);
+    } else if (guessesLeft < 2 && guessesLeft > 0) {
+        show(normalGif);
+    } else if (guessesLeft == 0) {
+        solution.innerHTML = challengeWord;
+        solution.style.color = "red";
+        lossCount += 1;
+        lossCountElement.innerHTML = lossCount;
+
+        show(loss);
+        show(resetButton);
+
+    }
+
+
+    // If word answered is correct
+    if (challengeWord == hangmanProgress.join("")) {
+        challengeWordElement.style.color = "blue";
+        winCount += 1;
+        winCountElement.innerHTML = winCount;
+        show(win);
+        show(resetButton);
+
+    }
+
+};
+
+
+// Reset function
+function reset() {
+    guessesLeft = 7;
+    challengeWord = "";
+    hangmanProgress = [];
+    userKeys = [];
+    guessesLeftElement.innerHTML = guessesLeft;
+    random = Math.floor(Math.random() * wordBank.length);
+    challengeWord = wordBank[random];
+    for (i = 0; i < challengeWord.length; i++) {
+        hangmanProgress.push(blankSpace);
+    }
+
+    challengeWordElement.innerHTML = hangmanProgress.join(" ");
+    guessesElement.innerHTML = userKeys.join(" ");
+    challengeWordElement.style.color = "black";
+
+    show(normalGif);
+    hide(win);
+    hide(loss);
+
+    solution.innerHTML = "";
+    console.log(hangmanProgress);
+ 
+}
+// reset on button click
+resetButton.onclick = function () {
+    reset();
+    hide(resetButton);
+}
+
+
+// show
+function show(image) {
+    image.style.display = "inline";
+}
+
+// hide
+function hide(image) {
+    image.style.display = "none";
+}
